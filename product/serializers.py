d38137076f4e982ee0822e300ca77d76fe374593
Anisul Haque <anisulhaque773@gmail.com>
2025-12-16 02:41:40 +0100

    serializers & api view

---
+++ product/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from decimal import Decimal
+from product.models import Category,Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name','description','product_count']
+        # fields = ['id','name','description']
+        
+    product_count = serializers.IntegerField()
+        
+    """Not Optimized method"""    
+    # product_count = serializers.SerializerMethodField(
+    #     method_name='get_product_count'
+    # )
+        
+    # def get_product_count(self,category):
+    #     count = Product.objects.filter(category=category).count()
+    #     return count
+
+
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     name = serializers.CharField()
+#     unit_price = serializers.DecimalField(max_digits=10,decimal_places=2,source = 'price')
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+#     # category = serializers.PrimaryKeyRelatedField(
+#     #     queryset = Category.objects.all()
+#     # )
+#     # category = serializers.StringRelatedField()
+#     # category = CategorySerializer()
+#     category = serializers.HyperlinkedRelatedField(
+#         queryset = Category.objects.all(),
+#         view_name = 'view-specific-category',
+#     )
+    
+#     def calculate_tax(self,product):
+#         return round(product.price*Decimal(1.1),2)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','name','description','price','stock','category','price_with_tax']
+     
+    # category = serializers.HyperlinkedRelatedField(
+    #     queryset = Category.objects.all(),
+    #     view_name = 'view-specific-category',
+    # )   
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax'
+    )   
+    def calculate_tax(self,product):
+        return round(product.price*Decimal(1.1),2)
+    
+    def validate_price(self,price):
+        if price<0:
+            raise serializers.ValidationError('Price could not be negative')
+        return price
+    
+    # just to know that this is a object level validation
+    # def validate(self, attrs):
+    #     if attrs['password1'!= 'password2']:
+    #         raise serializers.ValidationError("Password doesn't match")
--- product/views.py
+++ product/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from product.models import Product,Category
+from product.serializers import ProductSerializer,CategorySerializer
+from django.db.models import Count
 
 # Create your views here.
+
+@api_view(['GET','POST'])
+def view_products(request):
+    if request.method == 'GET':
+        products = Product.objects.select_related('category').all()
+        #  serializer = ProductSerializer(products, many=True,context = {'request':request}) 
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        """It do the opposite of serializer which is call deserializer"""
+        serializer = ProductSerializer(data = request.data)
+        # if serializer.is_valid():
+        #     print(serializer.validated_data)
+        #     serializer.save()
+        #     return Response(serializer.data,status=status.HTTP_201_CREATED)
+        # else:
+        #     return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+def view_specific_product(request,id):
+    if request.method == 'GET':
+        product = get_object_or_404(Product,pk=id)
+        serializer = ProductSerializer(product,context = {'request':request})
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        product = get_object_or_404(Product,pk = id)
+        serializer = ProductSerializer(product,data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        product = get_object_or_404(Product,pk=id)
+        copy_of_product = product
+        product.delete()
+        serializer = ProductSerializer(copy_of_product)
+        return Response(serializer.data,status=status.HTTP_204_NO_CONTENT)
+        
+    
+    
+    
+@api_view()
+def view_categories(request):
+    categories = Category.objects.annotate(product_count = Count('products')).all()
+    serializer = CategorySerializer(categories,many=True)
+    return Response(serializer.data)
+
+# @api_view(['GET','POST'])
+# def view_categories(request):
+#     if request.method == 'GET':
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories,many=True)
+#         return Response(serializer.data)
+#     if request.method == 'POST':
+#         serializer = CategorySerializer(data = request.data)
+#         if serializer.is_valid():
+#             print(serializer.validated_data)
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view()
+def view_specific_category(request,pk):
+    category = get_object_or_404(Category,pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
