2c9e4de795845c59fe23f91e201827c5991be830
Anisul Haque <anisulhaque773@gmail.com>
2025-12-22 02:50:53 +0100

    Permissions and Custom Permissions

--- product/views.py
+++ product/views.py
@@ -9,6 +9,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from product.filters import ProductFilter
 from rest_framework.filters import SearchFilter,OrderingFilter
 from product.paginations import DefaultPagination
+from rest_framework.permissions import IsAdminUser, AllowAny
+from api.permissions import IsAdminOrReadonly
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
@@ -20,7 +22,13 @@ class ProductViewSet(ModelViewSet):
     pagination_class = DefaultPagination
     search_fields = ['name','description']
     ordering_fields = ['price','updated_at']
+    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminOrReadonly]
     
+    # def get_permissions(self):
+    #     if self.request.method == "GET":
+    #         return [AllowAny()]
+    #     return [IsAdminUser()]
     # def get_queryset(self):
     #     queryset = Product.objects.all()
     #     category_id = self.request.query_params.get('category_id')
@@ -37,6 +45,7 @@ class ProductViewSet(ModelViewSet):
 
 
 class CategoryViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadonly]
     queryset = Category.objects.annotate(product_count = Count('products')).all()
     serializer_class = CategorySerializer
         
