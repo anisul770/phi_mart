deabd9716fd6dbbf930bddde5cbf7f9de6f257e2
Anisul Haque <anisulhaque773@gmail.com>
2025-12-22 01:46:17 +0100

    installed djoser and simple JWT

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'django_filters',
     'rest_framework',
+    'djoser',
     "api",
     "product",
     "users",
@@ -133,6 +134,13 @@ STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING' : False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     # 'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE' : 10
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
